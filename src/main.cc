68d71e9793cb33a6593b11dbe0d97c234fab1cf7
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-11-05 15:28:53 -0900

    add c

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,12 @@ set args = argv()
 if args[0] == "py" {
     python()
 }
+elif args[0] == "c" {
+    c()
+}
 elif args[0] == "-h" {
     print("python <project type>")
     print("\nWorking types:")
     print("   * py")
+    print("   * c")
 }
